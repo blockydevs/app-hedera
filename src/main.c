d245f316d4896e6b739f11b1a5778aa45a5f86cb
AntekWozniakBlocky <antoni.wozniak@blockydevs.com>
2025-04-24 14:19:11 +0200

    Removed unused commented out code

--- src/main.c
+++ src/main.c
@@ -42,7 +42,6 @@ void app_main() {
     volatile unsigned int tx = 0;
     volatile unsigned int flags = 0;
 
-    // reset_getpubkey_globals();
     reset_main_globals();
 
     for (;;) {
@@ -143,7 +142,6 @@ static void start_app_from_lib(void) {
     nbgl_useCaseSpinner("Signing");
 #endif  // HAVE_BAGL
     io_seproxyhal_init();
-    // nv_app_state_init();
     USB_power(0);
     USB_power(1);
 #ifdef HAVE_BLE
