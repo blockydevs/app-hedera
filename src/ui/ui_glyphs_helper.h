5e001d557c41fada39ff3f2617d62cd105d8733b
AntekWozniakBlocky <antoni.wozniak@blockydevs.com>
2025-09-19 11:08:01 +0200

    feat: add warning screen for NBGL

---
+++ src/ui/ui_glyphs_helper.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "glyphs.h"
+
+// Unify warning icon across devices:
+// - NBGL (Stax/Flex): use 64px warning icon provided by NBGL assets
+// - BAGL (Nano S+/X): use 14px app-provided warning icon
+#ifdef HAVE_NBGL
+    #define ICON_APP_WARNING C_Warning_64px
+#else
+    #define ICON_APP_WARNING C_icon_warning
+#endif
+
+
