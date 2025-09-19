c404badfb4bcf9026f81b5a2066ee4d9dbd4a11f
AntekWozniakBlocky <antoni.wozniak@blockydevs.com>
2025-09-19 13:21:36 +0200

    feat: add warning screen for BAGL

--- tests/standalone/utils.py
+++ tests/standalone/utils.py
@@ -21,9 +21,17 @@ def navigation_helper_reject(firmware, scenario_navigator):
 
 
 def navigate_erc20_confirm(firmware, navigator, scenario_navigator, screenshots_path: Path, test_name: str):
+    print(type(firmware))
     """Standard ERC20 navigation schema: capture NBGL warning, then approve review."""
     if firmware.is_nano:
-        scenario_navigator.review_approve()
+        print("navigate_erc20_confirm nano")
+        navigator.navigate_and_compare(
+            screenshots_path,
+            f"{test_name}_warning",
+            [NavInsID.RIGHT_CLICK, NavInsID.RIGHT_CLICK, NavInsID.RIGHT_CLICK],
+            screen_change_after_last_instruction=False,
+        )
+        scenario_navigator.review_approve(custom_screen_text="Confirm")
     else:
         navigator.navigate_and_compare(
             screenshots_path,
@@ -37,7 +45,14 @@ def navigate_erc20_confirm(firmware, navigator, scenario_navigator, screenshots_
 def navigate_erc20_reject(firmware, navigator, scenario_navigator, screenshots_path: Path, test_name: str):
     """Standard ERC20 navigation schema: capture NBGL warning, then reject review."""
     if firmware.is_nano:
-        scenario_navigator.review_reject()
+        print("navigate_erc20_reject nano")
+        navigator.navigate_and_compare(
+            screenshots_path,
+            f"{test_name}_warning",
+            [NavInsID.RIGHT_CLICK, NavInsID.RIGHT_CLICK, NavInsID.RIGHT_CLICK],
+            screen_change_after_last_instruction=False,
+        )
+        scenario_navigator.review_reject(custom_screen_text="Reject")
     else:
         navigator.navigate_and_compare(
             screenshots_path,
@@ -50,7 +65,14 @@ def navigate_erc20_reject(firmware, navigator, scenario_navigator, screenshots_p
 def navigate_erc20_reject_at_warning(firmware, navigator, scenario_navigator, screenshots_path: Path, test_name: str):
     """Standard ERC20 navigation schema: capture NBGL warning, then reject review."""
     if firmware.is_nano:
-        scenario_navigator.review_reject()
+        print("navigate_erc20_reject_at_warning nano")
+        navigator.navigate_and_compare(
+            screenshots_path,
+            f"{test_name}_warning",
+            [NavInsID.RIGHT_CLICK, NavInsID.RIGHT_CLICK, NavInsID.RIGHT_CLICK],
+            screen_change_after_last_instruction=False,
+        )
+        scenario_navigator.review_reject(custom_screen_text="Reject")
     else:
         navigator.navigate_and_compare(
             screenshots_path,
